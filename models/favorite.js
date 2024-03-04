52b362bd89da74832a98a11f06547840cec4cef4
dean-ferreira <deanferreira20@gmail.com>
2024-03-03 18:39:50 -0600

    Change type to object id

--- models/favorite.js
+++ models/favorite.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const favoriteSchema = new mongoose.Schema({
     user_id: {
-        type: String,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
         required: true,
     },
     user_email: {
